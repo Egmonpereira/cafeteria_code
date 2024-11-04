ac4a4bd1e6663b493020e0c124e08d86c767a6ae
egmonpereira <egmonpereira@gmail.com>
2024-11-04 11:17:30 -0300

    Atualização BD

--- bd_cafeteria_code.sql
+++ bd_cafeteria_code.sql
@@ -8,21 +8,21 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema cafeteria_gourmet
+-- Schema cafeteria_code
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema cafeteria_gourmet
+-- Schema cafeteria_code
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `cafeteria_gourmet` DEFAULT CHARACTER SET utf8 ;
-USE `cafeteria_gourmet` ;
+CREATE SCHEMA IF NOT EXISTS `cafeteria_code` DEFAULT CHARACTER SET utf8 ;
+USE `cafeteria_code` ;
 
 
 
 -- -----------------------------------------------------
--- Table `cafeteria_gourmet`.`cliente`
+-- Table `cafeteria_code`.`cliente`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cafeteria_gourmet`.`cliente` (
+CREATE TABLE IF NOT EXISTS `cafeteria_code`.`cliente` (
   `id_cliente` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(60) NOT NULL,
   `data_nasc` DATE NOT NULL,
@@ -34,9 +34,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `cafeteria_gourmet`.`endereco`
+-- Table `cafeteria_code`.`endereco`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cafeteria_gourmet`.`endereco` (
+CREATE TABLE IF NOT EXISTS `cafeteria_code`.`endereco` (
   `id_endereco` INT NOT NULL AUTO_INCREMENT,
   `cep` VARCHAR(8) NOT NULL,
   `rua` VARCHAR(100) NOT NULL,
@@ -49,16 +49,16 @@ CREATE TABLE IF NOT EXISTS `cafeteria_gourmet`.`endereco` (
   INDEX `fk_endereco_cliente1_idx` (`id_cliente` ASC),
   CONSTRAINT `fk_endereco_cliente1`
     FOREIGN KEY (`id_cliente`)
-    REFERENCES `cafeteria_gourmet`.`cliente` (`id_cliente`)
+    REFERENCES `cafeteria_code`.`cliente` (`id_cliente`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `cafeteria_gourmet`.`produto`
+-- Table `cafeteria_code`.`produto`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cafeteria_gourmet`.`produto` (
+CREATE TABLE IF NOT EXISTS `cafeteria_code`.`produto` (
   `id_produto` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(80) NOT NULL,
   `preco` FLOAT NOT NULL,
@@ -69,9 +69,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `cafeteria_gourmet`.`pedido`
+-- Table `cafeteria_code`.`pedido`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cafeteria_gourmet`.`pedido` (
+CREATE TABLE IF NOT EXISTS `cafeteria_code`.`pedido` (
   `id_pedido` INT NOT NULL AUTO_INCREMENT,
   `id_cliente` INT NOT NULL,
   `valor` FLOAT NOT NULL,
@@ -82,16 +82,16 @@ CREATE TABLE IF NOT EXISTS `cafeteria_gourmet`.`pedido` (
   INDEX `fk_pedido_cliente1_idx` (`id_cliente` ASC),
   CONSTRAINT `fk_pedido_cliente1`
     FOREIGN KEY (`id_cliente`)
-    REFERENCES `cafeteria_gourmet`.`cliente` (`id_cliente`)
+    REFERENCES `cafeteria_code`.`cliente` (`id_cliente`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `cafeteria_gourmet`.`pedido_produto`
+-- Table `cafeteria_code`.`pedido_produto`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cafeteria_gourmet`.`pedido_produto` (
+CREATE TABLE IF NOT EXISTS `cafeteria_code`.`pedido_produto` (
   `id_pedido` INT NOT NULL,
   `id_cliente` INT NOT NULL,
   `id_produto` INT NOT NULL,
@@ -101,21 +101,21 @@ CREATE TABLE IF NOT EXISTS `cafeteria_gourmet`.`pedido_produto` (
   INDEX `fk_pedido_has_produto_pedido1_idx` (`id_pedido` ASC, `id_cliente` ASC),
   CONSTRAINT `fk_pedido_has_produto_pedido1`
     FOREIGN KEY (`id_pedido` , `id_cliente`)
-    REFERENCES `cafeteria_gourmet`.`pedido` (`id_pedido` , `id_cliente`)
+    REFERENCES `cafeteria_code`.`pedido` (`id_pedido` , `id_cliente`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_pedido_has_produto_produto1`
     FOREIGN KEY (`id_produto`)
-    REFERENCES `cafeteria_gourmet`.`produto` (`id_produto`)
+    REFERENCES `cafeteria_code`.`produto` (`id_produto`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `cafeteria_gourmet`.`usuario`
+-- Table `cafeteria_code`.`usuario`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cafeteria_gourmet`.`usuario` (
+CREATE TABLE IF NOT EXISTS `cafeteria_code`.`usuario` (
   `id_usuario` INT NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(80) NOT NULL,
   `senha` VARCHAR(120) NOT NULL,
@@ -126,12 +126,12 @@ CREATE TABLE IF NOT EXISTS `cafeteria_gourmet`.`usuario` (
   INDEX `fk_usuario_cliente1_idx` (`id_cliente` ASC),
   CONSTRAINT `fk_usuario_cliente1`
     FOREIGN KEY (`id_cliente`)
-    REFERENCES `cafeteria_gourmet`.`cliente` (`id_cliente`)
+    REFERENCES `cafeteria_code`.`cliente` (`id_cliente`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
-CREATE USER IF NOT EXISTS `cafeteria_gourmet`@`localhost`;
-GRANT ALL PRIVILEGES ON `cafeteria_gourmet`.`*` TO `cafeteria_gourmet`@`localhost` IDENTIFIED BY 'qwe123';
+CREATE USER IF NOT EXISTS 'cafeteria_code'@'localhost';
+GRANT ALL PRIVILEGES ON cafeteria_code.* TO 'cafeteria_code'@'localhost' IDENTIFIED BY 'qwe123';
 INSERT INTO produto (nome, preco, descricao, img) VALUES ('CAFÉ SUPER UM', 8.30, 'CAPSULA 10 UNIDADES', 'cf_01.webp');
 INSERT INTO produto (nome, preco, descricao, img) VALUES ('CAFÉ SUPER DOIS', 12.20, 'CAPSULA 15 UNIDADES', 'cf_02.webp');
 INSERT INTO produto (nome, preco, descricao, img) VALUES ('CAFÉ SUPER TRES', 20.50, 'CAPSULA 20 UNIDADES', 'cf_03.webp');
